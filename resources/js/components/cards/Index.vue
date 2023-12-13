4e334bb31b3149c7211aa880077af8c8cc33508a
daniel-jedrasiewicz <jedrasiewicz.daniel@gmail.com>
2023-12-13 18:31:03 +0100

    Add cards index vue component

---
+++ resources/js/components/cards/Index.vue
@@ -0,0 +1,72 @@
+<template>
+    <div class="overflow-hidden overflow-x-auto p-6 bg-white border-gray-200">
+        <div class="min-w-full align-middle">
+            <table class="min-w-full divide-y divide-gray-200 border">
+                <thead>
+                <tr>
+                    <th class="px-6 py-3 bg-gray-50 text-left">
+                        <span class="text-xs leading-4 font-medium text-gray-500 uppercase tracking-wider">ID</span>
+                    </th>
+                    <th class="px-6 py-3 bg-gray-50 text-left">
+                        <span class="text-xs leading-4 font-medium text-gray-500 uppercase tracking-wider">Card Number</span>
+                    </th>
+                    <th class="px-6 py-3 bg-gray-50 text-left">
+                        <span class="text-xs leading-4 font-medium text-gray-500 uppercase tracking-wider">PIN</span>
+                    </th>
+                    <th class="px-6 py-3 bg-gray-50 text-left">
+                        <span class="text-xs leading-4 font-medium text-gray-500 uppercase tracking-wider">Activation Date</span>
+                    </th>
+                    <th class="px-6 py-3 bg-gray-50 text-left">
+                        <span class="text-xs leading-4 font-medium text-gray-500 uppercase tracking-wider">Expiration Date</span>
+                    </th>
+                    <th class="px-6 py-3 bg-gray-50 text-left">
+                        <span class="text-xs leading-4 font-medium text-gray-500 uppercase tracking-wider">Balance</span>
+                    </th>
+                </tr>
+                </thead>
+                <tbody class="bg-white divide-y divide-gray-200 divide-solid">
+                <tr v-for="card in cards" :key="card.id">
+                    <td class="px-6 py-4 whitespace-no-wrap text-sm leading-5 text-gray-900">
+                        #{{ card.id }}
+                    </td>
+                    <td class="px-6 py-4 whitespace-no-wrap text-sm leading-5 text-gray-900">
+                        {{ card.card_number }}
+                    </td>
+                    <td class="px-6 py-4 whitespace-no-wrap text-sm leading-5 text-gray-900">
+                        {{ card.pin }}
+                    </td>
+                    <td class="px-6 py-4 whitespace-no-wrap text-sm leading-5 text-gray-900">
+                        {{ card.activation_date }}
+                    </td>
+                    <td class="px-6 py-4 whitespace-no-wrap text-sm leading-5 text-gray-900">
+                        {{ card.expiration_date }}
+                    </td>
+                    <td class="px-6 py-4 whitespace-no-wrap text-sm leading-5 text-gray-900">
+                        {{ card.balance }}
+                    </td>
+                </tr>
+                </tbody>
+            </table>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            cards: []
+        }
+    },
+    mounted() {
+        this.fetchCards()
+    },
+    methods: {
+        fetchCards() {
+            axios.get('/api/cards')
+                .then(response => this.cards = response.data.data)
+                .catch(error => console.log(error))
+        }
+    }
+}
+</script>
